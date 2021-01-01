429b3c9c625369eca1e81d27d1e3594f5c638012
vorachet <vorachet@gmail.com>
2021-01-02 04:44:58 +0700

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# mdeshell
+
+mdeshell is a shell script based modeling environment that facilitates the definition of XML-based EMF metamodels/models and the execution of Model-2-Text transformation based on Model-Driven Engineering system im- plemented by the Eclipse Emfatic and the Epsilon Flexmi/EGL/EGX.
+
+Project website: https://github.com/vorachet/mdeshell
+
+Maintainer: Vorachet Jaroensawas <vorachet@gmail.com> @aotvorachet
+
+Limitation: mdeshell is tested on macOS 11.1/Linux Ubuntu 18 machine and Java 13.0.1. mdeshell does not provide an environment for MS Windows users and tutorial material at this time
+
+# Documentation
+
+See readme.pdf
---
+++ run.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# Vorachet Jaroensawas <vorachet@gmail.com>
+
+printf "\nChoose project number:\n"
+cd projects
+select d in * ; do test -n "$d" && break; echo ">>> Invalid Selection"; done
+project=$d
+
+cd ../
+
+projectBasePath=$(pwd)/projects/${project}
+mkdir -p ${projectBasePath}/outputs
+emfaticPath=${projectBasePath}/inputs/metamodel.emf
+firstGeneratedEcorePath=${projectBasePath}/inputs/metamodel.ecore
+tempEcorePath=${projectBasePath}/outputs/metamodel.ecore
+echo "\nProject: ${project} (${projectBasePath})"
+java -jar libs/Emfatic2Ecore.jar ${emfaticPath} 
+mv ${firstGeneratedEcorePath} ${projectBasePath}/outputs/
+ 
+#echo "\tEMFATIC ${emfaticPath}"
+#echo "\tGENERATED ECORE ${tempEcorePath}"
+
+echo "\nGenerating..."
+
+for flexmiFilePath in "$projectBasePath"/inputs/*.flexmi
+do
+  name=$(basename "$flexmiFilePath" ".flexmi")
+  #echo "\tMODEL: ${flexmiFilePath}"
+  #echo "\tGENERATED XMI: ${projectBasePath}/inputs/${name}.flexmi"
+  java -jar libs/Flexmi2Xmi.jar ${tempEcorePath} ${flexmiFilePath} ${projectBasePath}/outputs/${name}.xmi
+done
+
+
+for egxProgramPath in "$projectBasePath"/inputs/*.egx
+do
+  #echo "\tEGX PROGRAM: ${egxProgramPath}"
+  for xmiFilePath in "${projectBasePath}"/outputs/*.xmi
+  do
+    #echo "\tEGX PROGRAM tempEcorePath: ${tempEcorePath}"
+    #echo "\tEGX PROGRAM xmiFilePath: ${xmiFilePath}"
+    java -jar libs/EGXRunner.jar ${egxProgramPath} ${tempEcorePath} ${xmiFilePath}
+  done
+done
+
+echo "\nDone! Note that location of generated files will be specified by your .egx files\n"
+
+exit
